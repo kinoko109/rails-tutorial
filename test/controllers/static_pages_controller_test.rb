babc6f6e3684253ea264d1b6c1970e54906b63e9
kinoko109 <tatsuaki1009@gmail.com>
2022-04-26 00:43:27 +0900

    contact page add, test fix

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -15,9 +15,15 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
+#   test "should get about" do
+#     get static_pages_about_url
+#     assert_response :success
+#     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+#   end
+
   test "should get about" do
-    get static_pages_about_url
+    get static_pages_contact_url
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 end
