d3f65e0ae78cdeadaf105c11fc2ad3f67d2dc1fa
kinoko109 <tatsuaki1009@gmail.com>
2022-04-29 23:21:24 +0900

    migrate reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_17_064959) do
-
-  create_table "microposts", force: :cascade do |t|
-    t.text "content"
-    t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2022_04_29_133131) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
