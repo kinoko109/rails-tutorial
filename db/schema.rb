f4a88d1ba5233e264f33028255732fbb0937775c
kinoko109 <tatsuaki1009@gmail.com>
2022-04-30 01:16:10 +0900

    email column add index

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_29_133131) do
+ActiveRecord::Schema.define(version: 2022_04_29_155544) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
