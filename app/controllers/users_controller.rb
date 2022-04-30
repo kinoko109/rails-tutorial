cf41f8af1ebcbd8b2e16c8f57265d06b3b2a6c79
kinoko109 <tatsuaki1009@gmail.com>
2022-04-30 20:43:51 +0900

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,26 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def show
     @user = User.find(params[:id])
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # 保存処理
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      # 対象のパラメーターだけを許可する（このように指定しないと他のパラメーターも受け付けてしまい脆弱性につながる）
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
