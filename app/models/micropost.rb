ce11cc9744a614c5d58914c3d947f3ed21c7e4a9
kinoko109 <tatsuaki1009@gmail.com>
2024-03-04 23:33:13 +0900

    feat: 13.1

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,7 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  # -> という文法はラムダ式の書き方。Proc,lambdaオブジェクトを作成する。
+  default_scope -> { order(created_at: :desc) }
+  validates :user_id, presence: true
+  validates :content, presence: true, length: { maximum: 140 }
 end
